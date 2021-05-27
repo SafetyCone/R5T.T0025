0d0a4cf01df0af3785e231abcd50e32bcc7a7f71
David Coats <david@getsafetycone.com>
2021-05-27 10:40:51 -0700

    Add R5T.Stagira project-reference dependency. Add DataSourceName typed string.

---
+++ source/R5T.T0025/Code/Classes/DataSourceName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.T0025
+{
+    public class DataSourceName : TypedString
+    {
+        public DataSourceName(string value)
+            : base(value)
+        {
+        }
+    }
+}
